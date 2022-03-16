aab152fe46bad8422c6cf625009cf3ebdb560efe
C4illin <emrik.ostling@gmail.com>
2022-03-16 20:08:15 +0100

    added discord notification

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const puppeteer = require("puppeteer")
-const fs = require("fs");
+import launch from "puppeteer"
+import fetch from "node-fetch"
 
-// https://bokapass.nemoq.se/Booking/Booking/Index/vastragotaland
 async function getNewTime() {
-  const browser = await puppeteer.launch({
+  const browser = await launch({
     args: ["--disable-dev-shm-usage","--no-sandbox"]
   })
   const page = await browser.newPage()
@@ -22,13 +21,42 @@ async function getNewTime() {
     return document.querySelector("#datepicker").value
   })
   console.log(date)
-  const text = await page.evaluate(() => {
+  let month = date.split("-")[1]
+  let text = await page.evaluate(() => {
     return document.querySelector("div.container-fluid:nth-child(7)").innerText
   })
-  fs.writeFileSync(date+".txt", text)
+
+  let tsplit = text.split(/\n(?=[A-Z|Å|Ä|Ö])/).filter(x => x.includes(":"))
+  let towrite = tsplit.join("\n").replaceAll(/\n+/g, "\n")
+
+  if (month < 7) {
+    let params = {
+      "content": null,
+      "embeds": [
+        {
+          "title": date,
+          "description": towrite,
+          "color": 65402,
+          "author": {
+            "name": "Boka",
+            "url": "https://bokapass.nemoq.se/Booking/Booking/Index/vastragotaland/"
+          }
+        }
+      ]
+    }
+    fetch("https://discord.com/api/webhooks/953727356722360330/9qd8Q7cjgN5l5utv8S2z22XszI11Ky7yo_275mQOzxwBv25fpzZjonqlpg_11La7NW29", {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    })
+  }
+  
+  // fs.writeFileSync(date+".txt", towrite)
   // await page.screenshot({path: "example.png"})
   await browser.close()
 }
 
 getNewTime()
-
+setInterval(getNewTime, 5 * 60 * 1000) //Runs every five minutes
